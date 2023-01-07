fa7c0f53c89c4f90005f1a37e42d08683dea693c
Prince2347X <prince.raj29042004@gmail.com>
2023-01-08 02:32:02 +0530

    Implemented Splash And Launcher Icon

--- lib/screens/diet.dart
+++ lib/screens/diet.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-class DietScreen extends StatelessWidget {
+class DietScreen extends StatefulWidget {
   const DietScreen({Key? key}) : super(key: key);
 
   @override
+  State<DietScreen> createState() => _DietScreenState();
+}
+
+class _DietScreenState extends State<DietScreen> {
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            body: Column()
-        )
+    return FittedBox(
+      fit: BoxFit.cover,
+      child: Image.asset("assets/diet_bg.png"),
     );
   }
 }
--- lib/screens/meditation.dart
+++ lib/screens/meditation.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MeditationScreen extends StatelessWidget {
+class MeditationScreen extends StatefulWidget {
   const MeditationScreen({Key? key}) : super(key: key);
 
   @override
+  State<MeditationScreen> createState() => _MeditationScreenState();
+}
+
+class _MeditationScreenState extends State<MeditationScreen> {
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            body: Column()
-        )
+    return FittedBox(
+      fit: BoxFit.cover,
+      child: Image.asset("assets/meditate_bg.png"),
     );
   }
 }
